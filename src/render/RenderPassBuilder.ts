3e2ce3a0d182aa2e34db150ad5ac492cc057f932
Irrgh <schellenberger.erik@gmail.com>
2024-11-29 00:01:20 +0100

    changed structure of colorAttachments

--- src/render/RenderPassBuilder.ts
+++ src/render/RenderPassBuilder.ts
@@ -2,7 +2,7 @@ import { PassBuilder, ResourceAccess } from "./PassBuilder";
 
 export class RenderPassBuilder extends PassBuilder {
 
-    private colorAttachments: Map<number,{name:string,access:ResourceAccess}>;
+    private colorAttachments: Map<string,ResourceAccess>;
     private depthAttachment?: {name:string,access:ResourceAccess};
 
 
@@ -19,17 +19,21 @@ export class RenderPassBuilder extends PassBuilder {
 
     /**
      * Adds a texture as a color attachment. Shader layout will follow the insertion order.
-     * @param name name of the texture
-     * @param access used to manage `loadOp` and `storeOp` in the actual attachment
-     * @param slot 
+     * @param name name of the texture to be used as a color attachment.
+     * @param access used to manage `loadOp` and `storeOp` in the actual attachment.
      */
-    public addColorAttachment(name:string,access:ResourceAccess, slot:number) {
-        if (this.colorAttachments.has(slot)) {
-            throw new Error(`ColorAttachment slot ${slot} is already take`)
+    public addColorAttachment(name:string,access:ResourceAccess) {
+        if (this.colorAttachments.has(name)) {
+            throw new Error(`Color attachment declaration error: [${name}] is already used as color attachment.`)
         }
-        this.colorAttachments.set(slot,{name,access})
+        this.colorAttachments.set(name,access);
     }
 
+    /**
+     * Adds a texture a depth Attachment.
+     * @param name name of the texture to be used as a depth attachment.
+     * @param access used to manage `loadOp` and `storeOp` in the actual attachment.
+     */
     public setDepthAttachment(name:string,access:ResourceAccess) {
         this.depthAttachment =  {name,access};
     }
